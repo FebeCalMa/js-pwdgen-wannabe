834522efad26501fb5bb4bdd0ff6678d16daaebd
Febe Calderon <samcalma1@gmail.com>
2022-03-17 16:51:37 +0100

    richieste al utente

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("JS OK!");
+
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina e/o in console nomecognomecolorepreferito21
+*/
+
+//Variabili da chiedere
+  //const nome;
+  //const cognome;
+  //const colore preferito;
+
+//Chiedere nome
+const nome = prompt("Qual'è il tuo nome?");
+
+//Chiedere il cognome
+const cognome = prompt("Qual'è il tuo cognome?");
+
+//Chiedere colore preferito
+const colorePreferito;
