46fab7c4b7da89b6915a5daccdc2becb9c6fffb4
Febe Calderon <samcalma1@gmail.com>
2022-03-17 18:26:52 +0100

    password generator

--- script.js
+++ script.js
@@ -8,15 +8,33 @@ Infine scrivi sulla pagina e/o in console nomecognomecolorepreferito21
 */
 
 //Variabili da chiedere
-  //const nome;
-  //const cognome;
-  //const colore preferito;
+//const nome;
+//const cognome;
+//const colore preferito;
 
 //Chiedere nome
 const nome = prompt("Qual'è il tuo nome?");
+//console.log(nome);
 
 //Chiedere il cognome
 const cognome = prompt("Qual'è il tuo cognome?");
+//console.log(cognome);
 
 //Chiedere colore preferito
-const colorePreferito;
+const colorePreferito = prompt("Qual'è il tuo colore preferito?");
+//console.log(colorePreferito);
+
+//Numero 21
+const numero = 21;
+//console.log(numero);
+
+//Password Creator
+const password = nome + cognome + colorePreferito + numero;
+//pagina
+document.getElementById("password-generated").innerHTML =
+  "La tua password è:" + " " + password;
+//console
+console.log(
+  (document.getElementById("password-generated").innerHTML =
+    "La tua password è:" + " " + password)
+);
